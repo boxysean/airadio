0e5e892fc9e5a14dddfa4a8a8530286437646719
boxysean <boxysean@gmail.com>
2012-09-10 05:55:02 -0400

    minor update

--- air-download.py
+++ air-download.py
@@ -4,13 +4,19 @@ import sys
 
 from optparse import OptionParser
 
-def getMsgs(servername="imap.gmail.com"):
-  usernm = "interactivosradio"
-  passwd = getpass.getpass()
+def getMsgs(usernm, passwd=None, servername="imap.gmail.com", first=True):
+  if not passwd:
+    passwd = getpass.getpass()
+
   conn = imaplib.IMAP4_SSL(servername, 993)
   conn.login(usernm,passwd)
   conn.select()
-  typ, data = conn.search(None,'ALL')
+
+  if first:
+    typ, data = conn.search(None,'ALL')
+  else:
+    typ, data = conn.search(None,'UnSeen')
+
   for num in data[0].split():
     typ, data = conn.fetch(num,'(RFC822)')
     msg = email.message_from_string(data[0][1])
@@ -19,28 +25,33 @@ def getMsgs(servername="imap.gmail.com"):
 
 def getAttachment(msg):
   for part in msg.walk():
-    print part.get_content_type()
+    # return only the first attachment...
     if part.get_content_type().startswith("audio"):
       return part.get_filename(), part.get_payload(decode=1)
 
 if __name__ == '__main__':
   parser = OptionParser()
 
-  parser.add_option("-f", "--first", dest="first_run", help="checks all emails in the account")
+  parser.add_option("-c", "--config-file", dest="config_file", help="location of config file")
+  parser.add_option("-f", "--first", action="store_true", dest="first_run", help="checks all emails in the account", default=False)
 
   (options, args) = parser.parse_args()
 
-#  print options, args
-#  sys.exit(0)
+  config = yaml.load(open(options.config_file, "r"))
 
-  for msg in getMsgs(options["account"]):
+  for msg in getMsgs(config["account"], passwd=config["password"], servername=config["server"], first=options.first_run):
     filename, payload = getAttachment(msg)
 
     if not payload:
       continue
 
-    print "writing %s" % (filename)
-    fp = open(filename, 'wb')
-    fp.write(payload)
-    fp.close()
+    filepath = config["destination_folder"] + os.sep + filename
+
+    # Fri, 7 Sep 2012 12:28:21 +0200
+    print "from date", msg["date"], type(msg["date"])
+    if not os.path.isfile(filepath):
+      print "writing %s" % (filename)
+      fp = open(filepath, 'wb')
+      fp.write(payload)
+      fp.close()
 
